06c7cdc9306e93709743ad7360b73b4bc1163d71
amada22 <elbouzarimohamed241@gmail.com>
2026-06-19 16:29:09 +0100

    deploying db with aws

--- app/page.js
+++ app/page.js
@@ -35,14 +35,9 @@ function formatTimer(milliseconds) {
 }
 
 function formatTime(value) {
-  if (!value) {
-    return "--:--";
-  }
+  if (!value) return "--:--";
 
-  return new Intl.DateTimeFormat("en-GB", {
-    hour: "2-digit",
-    minute: "2-digit",
-  }).format(new Date(value));
+  return value.slice(0, 5);
 }
 
 function getLiveWorkedMilliseconds(shift, status, now) {
